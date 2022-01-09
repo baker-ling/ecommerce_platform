49ccb7ffa21c34a724abf0a7f5fa3926145e4615
Brian D. Baker <brian.devs.baker@gmail.com>
2022-01-09 11:23:00 -0500

    Missed store and selector files in last commit

---
+++ client/src/utils/selectors.js
@@ -0,0 +1,5 @@
+export const selectProducts = state => state.products;
+export const selectCart = state => state.cart;
+export const selectCurrentCategory = state => state.currentCategory;
+export const selectCategories = state => state.categories;
+export const selectCartOpen = state => state.cartOpen;
